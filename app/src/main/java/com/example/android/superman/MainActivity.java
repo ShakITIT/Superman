7b0a46618dd5b583c6fae512d117956b9cd840dd
ShakITIT <ShakTS@outlook.com>
2018-04-15 07:38:34 -0400

    Done and formatted

--- app/src/main/java/com/example/android/superman/MainActivity.java
+++ app/src/main/java/com/example/android/superman/MainActivity.java
@@ -1,4 +1,5 @@
 package com.example.android.superman;
+
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -6,6 +7,7 @@ import android.widget.CheckBox;
 import android.widget.RadioButton;
 import android.widget.TextView;
 import android.widget.Toast;
+
 /**
  * This is a Quiz app that tests your knowledge of Superman.
  */
@@ -14,31 +16,32 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toast.makeText(getApplicationContext(),"Up, Up and Away!",Toast.LENGTH_SHORT).show();
+        Toast.makeText(getApplicationContext(), "Up, Up and Away!", Toast.LENGTH_SHORT).show();
     }
-    public void checkAnswers(View view){
+
+    public void checkAnswers(View view) {
         int count = 0; // count of correct answers
         // correct answers
-        RadioButton q1correct = (RadioButton)findViewById(R.id.yes_radio_button);
+        RadioButton q1correct = (RadioButton) findViewById(R.id.yes_radio_button);
         boolean q1 = q1correct.isChecked();
-        if (q1){
+        if (q1) {
             count += 1;
         }
-        RadioButton q2correct = (RadioButton)findViewById(R.id.yes2_radio_button);
+        RadioButton q2correct = (RadioButton) findViewById(R.id.yes2_radio_button);
         boolean q2 = q2correct.isChecked();
-        if (q2){
+        if (q2) {
             count += 1;
         }
-        RadioButton q3correct = (RadioButton)findViewById(R.id.yes3_radio_button);
+        RadioButton q3correct = (RadioButton) findViewById(R.id.yes3_radio_button);
         boolean q3 = q3correct.isChecked();
-        if (q3){
+        if (q3) {
             count += 1;
         }
         CheckBox q4correct = (CheckBox) findViewById(R.id.checkbox_Jerry);
         CheckBox q4correct_2 = (CheckBox) findViewById(R.id.checkbox_Joe);
-        boolean q4_2 =  q4correct_2.isChecked();
+        boolean q4_2 = q4correct_2.isChecked();
         boolean q4 = q4correct.isChecked();
-        if(q4 && q4_2) {
+        if (q4 && q4_2) {
             count += 1;
 
         }
@@ -46,7 +49,7 @@ public class MainActivity extends AppCompatActivity {
         // TextView finalMessage = (TextView) findViewById(R.id.);
         // finalMessage.setText(getString(R.string.correctMessage, Integer.toString(count)));
 
-        Toast.makeText(getApplicationContext(),"Your Score is:" +count, Toast.LENGTH_SHORT).show();
+        Toast.makeText(getApplicationContext(), "Your Score is:" + count, Toast.LENGTH_SHORT).show();
     }
 }
 
